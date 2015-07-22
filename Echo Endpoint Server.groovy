fe7926a7fb60474467f48f9e5c4fd4d7529fa023
mitchpond <mitchp@gmail.com>
2015-07-22 05:49:35 +0000

    C9 broke formatting. Lack of semicolons caused issues...

--- Echo Endpoint Server.groovy
+++ Echo Endpoint Server.groovy
@@ -26,7 +26,7 @@ definition(
 
 
 preferences {
-    section("Choose devices use with Alexa...") {
+	section ("Choose devices use with Alexa...") {
         input "switches", "capability.switch", title: "Which switches?", multiple: true, required: false
         input "dimmers", "capability.switchLevel", title: "Which dimmers?", multiple: true, required: false
         input "contact", "capability.contactSensor", title: "Which contact sensors?", multiple: true, required: false
@@ -34,49 +34,35 @@ preferences {
 }
 
 mappings {
-    path("/getSwitches") {
-        action: [GET: "listSwitches"]
-    }
-    path("/getSwitches/:device") {
-        action: [GET: "getSwitchState"]
-    }
-    path("/switch/:device/:command") {
-        action: [GET: "setSwitchState"]
-    }
-    path("/getContact") {
-        action: [GET: "listContactSensors"]
-    }
-    path("/getContact/:device") {
-        action: [GET: "getContactState"]
-    }
-    path("/getState/:device") {
-        action: [GET: "getDeviceState"]
-    }
-    path("/dimmer/:device/:level") {
-        action: [GET: "setDimmerLevel"]
-    }
+	path("/getSwitches") 				{action: [GET: "listSwitches"]}
+    path("/getSwitches/:device")		{action: [GET: "getSwitchState"]}
+    path("/switch/:device/:command")	{action: [GET: "setSwitchState"]}
+    path("/getContact") 				{action: [GET: "listContactSensors"]}
+    path("/getContact/:device") 		{action: [GET: "getContactState"]}
+    path("/getState/:device")			{action: [GET: "getDeviceState"]}
+    path("/dimmer/:device/:level")		{action: [GET: "setDimmerLevel"]}
 }
 def installed() {
-    log.debug "Installed with settings: ${settings}"
+	log.debug "Installed with settings: ${settings}"
 
-    initialize()
+	initialize()
 }
 
 def updated() {
-    log.debug "Updated with settings: ${settings}"
+	log.debug "Updated with settings: ${settings}"
 
-    unsubscribe()
-    initialize()
+	unsubscribe()
+	initialize()
 }
 
 def initialize() {
-    // TODO: subscribe to attributes, devices, locations, etc.
+	// TODO: subscribe to attributes, devices, locations, etc.
 }
 
 // TODO: implement event handlers
 
 def listSwitches() {
-    def response = []
+	def response = []
     switches.each {
         response << [name: it.displayName, state: it.currentValue("switch")]
     }
@@ -84,52 +70,39 @@ def listSwitches() {
 }
 
 def getSwitchState() {
-    def device = switches.find({
-        params.device.toLowerCase() == it.label.toLowerCase()
-    })
+	def device = switches.find({params.device.toLowerCase() == it.label.toLowerCase()})
     def response = []
-
-    return [name: device.displayName, state: device.currentValue("switch")]
+    
+    return [name: device.displayName, state:device.currentValue("switch") ]
 }
 
 def setSwitchState() {
-    def device = (switches.find({
-        params.device.toLowerCase() == it.label.toLowerCase()
-    })) ? it : (dimmers.find({
-        params.device.toLowerCase() == it.label.toLowerCase()
-    }))
+	def device = (switches.find({params.device.toLowerCase() == it.label.toLowerCase()})) ? it : (dimmers.find({params.device.toLowerCase() == it.label.toLowerCase()}))
     if (device) {
-        def cmd = params.command(cmd == 'on') ? device ? .on() : (cmd == 'off') ? device ? .off() : [name: 'test', result: 'failure']
-        return [name: device.displayName, state: device.currentValue("switch")]
-    }
-    else return null
+    	def cmd = params.command
+    	(cmd == 'on') ? device?.on() : (cmd == 'off') ? device?.off() : [name: 'test', result: 'failure']
+    	return [name: device.displayName, state:device.currentValue("switch") ]}
+     else return null
 }
 
 def setDimmerLevel() {
-    def device = dimmers.find({
-        params.device.toLowerCase() == it.label.toLowerCase()
-    })
+	def device = dimmers.find({params.device.toLowerCase() == it.label.toLowerCase()})
     def level = Integer.parseInt(params.level)
-        (level >= 0 && level <= 100) ? device ? .setLevel(level) : [name: 'test', result: 'failure']
-    return [name: device.displayName, state: device.currentValue("level")]
+    (level >= 0 && level <= 100) ? device?.setLevel(level) : [name: 'test', result: 'failure']
+    return [name: device.displayName, state:device.currentValue("level") ]
 }
 
 def getDeviceState() {
-    def deviceName = params.device
-    def allDevices = (switches + contact + dimmers).collect()
-    def myDevice = allDevices.find({
-        it ? .label ? .equalsIgnoreCase(deviceName.toLowerCase())
-    })
+	def deviceName = params.device
+    def allDevices = (switches+contact+dimmers).collect()
+    def myDevice = allDevices.find({it?.label?.equalsIgnoreCase(deviceName.toLowerCase())})
     try {
-        def deviceState = []
-        deviceState << myDevice.switchState
-        deviceState << (myDevice.levelState ? [value: "${myDevice.levelState.value}%"] : myDevice.levelState)
-        deviceState << myDevice.contactState
-        def result = []
-        result += deviceState * .value
-        return [name: myDevice.label, state: result]
-    }
-    catch (e) {
-        return
-    }
+	def deviceState = []
+    deviceState << myDevice.switchState 
+    deviceState << (myDevice.levelState ? [value: "${myDevice.levelState.value}%"] : myDevice.levelState)
+    deviceState << myDevice.contactState
+    def result = []
+    result += deviceState*.value
+    return [name: myDevice.label, state: result]}
+    catch(e){return}
 }
